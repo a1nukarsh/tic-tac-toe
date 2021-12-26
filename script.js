4bd86bc2889d1475869468fd070c0d5299f414d3
Anukarsh <anukarshr@gmail.com>
2021-12-26 23:38:45 +0530

    update

--- script.js
+++ script.js
@@ -9,7 +9,33 @@ const changeTurn = ()=>{
 
 // Function to check if won
 const checkWin = ()=>{
+    let boxTexts = document.getElementsByClassName('boxText')
+    let wins = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 5, 7],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+    wins.forEach(e =>{
+        
+    })
 
 }
 
 // Game Logic
+let boxes  = document.getElementsByClassName('box')
+Array.from(boxes).forEach(element =>{
+    let boxText = element.querySelector('.boxText')
+    element.addEventListener('click', ()=>{
+        if (boxText.innerText === ''){
+            boxText.innerText = turn;
+            turn = changeTurn()
+            checkWin()
+            document.getElementsByClassName('info')[0].innerText = "Turn for " + turn
+        }
+    })
+})

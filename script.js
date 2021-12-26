10a8f481b111d3c3ac8adac858ebd5951dd9a617
Anukarsh <anukarshr@gmail.com>
2021-12-26 23:22:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ console.log("This is a tic tac toe game!")
 let turn = "X"
 
 // Function to change the turn
-const changeTurn
+const changeTurn = ()=>{
+    return turn === "X"?"0":"X"
+}
+
+// Function to check if won
+const checkWin = ()=>{
+
+}
+
+// Game Logic
